460034f2a45d0eb463cf415bb7d12daade08b937
amirgal <amirc538@gmail.com>
2020-02-17 10:38:43 +0200

    root commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+var bodyParser = require('body-parser')
+const path = require('path')
+const api = require('./server/routes/api')
+const app = express()
+
+
+app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, 'node_modules')))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use('/', api)
+
+const port = 3000
+app.listen(port, function () {
+    console.log(`Running server on port ${port}`)
+})
